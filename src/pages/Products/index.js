827429c90bd61a1cc2c01501c65e0418ebc12395
victrosantos <victor.santos.fk@hotmail.com>
2023-08-16 10:09:00 -0300

    feat: Nome dos items aparece no frontend

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,3 +1,4 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { collection, getDocs } from 'firebase/firestore/lite';
 import React, { useEffect, useState } from "react";
 import { FlatList, Text, TouchableOpacity, View } from "react-native";
@@ -6,6 +7,10 @@ import styles from './style';
 
 export default function Products({navigation}) {
   const [products, setProducts] = useState([]);
+
+  const addToCart = (item) => {
+    console.log(item);
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -29,17 +34,26 @@ export default function Products({navigation}) {
       <FlatList 
         showsVerticalScrollIndicator={false}
         data={products}
-        renderItem={(item) => {
+        renderItem={({item}) => {
+          return(
           <View style={styles.viewProducts}>
             <TouchableOpacity
               style={styles.addToCart}
-              onPress={addToCart}
+              onPress={() => addToCart(item)}
             >
-              <Text style={styles.iconButton}>
-                +
+              <FontAwesome
+                name='star'
+                size={23}
+                color="#F92E6A"
+              />
+              <Text
+                style={styles.nameText}
+              >
+                {item.name}
               </Text>
             </TouchableOpacity>
           </View>
+          )
         }}
       />
       <TouchableOpacity
